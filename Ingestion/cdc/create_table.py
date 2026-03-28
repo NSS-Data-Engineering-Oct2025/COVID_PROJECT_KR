71630842198252eb00d798e3bae519fdff3b7c80
kaylamckenzie <kmckenzie82@bethelu.edu>
2026-03-28 12:37:14 -0500

    Ingest_vaccination_coverage.py

--- Ingestion/cdc/create_table.py
+++ Ingestion/cdc/create_table.py
@@ -5,7 +5,7 @@ import os
 load_dotenv()
 
 API_TOKEN = os.getenv("CDC_API_TOKEN")
-URL = "https://data.cdc.gov/resource/rh2h-3yt2.json"
+URL = "https://data.cdc.gov/resource/ksfb-ug5d.json"
 
 params = {"$limit": 1}
 headers = {"X-App-Token": API_TOKEN}
@@ -21,7 +21,7 @@ for col in columns:
 print("]")
 
 print("\n\nCREATE TABLE:")
-print("CREATE TABLE IF NOT EXISTS covid_pipeline.RAW.VACCINATION_TRENDS (")
+print("CREATE TABLE IF NOT EXISTS covid_pipeline.RAW.VACCINATION_COVERAGE (")
 for i, col in enumerate(columns):
     comma = "," if i < len(columns) - 1 else ""
     print(f"    {col} VARCHAR{comma}")
